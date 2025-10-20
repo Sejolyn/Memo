2e5f28b206b7594beb752906ca99328cfa457cf5
sijolin <dev.sijolin@outllook.com>
2025-10-20 20:01:45 +0800

    Update site links

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // === Basic configuration ===
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Astro Theme Pure',
+  title: "The Sejolyn Memo",
   /** Will be used in index page & copyright declaration */
-  author: 'Pure Lab',
+  author: 'Sejolyn',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: 'So fucking close.',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
@@ -48,7 +48,6 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: 'Blog', link: '/blog' },
-      { title: 'Docs', link: '/docs' },
       { title: 'Projects', link: '/projects' },
       { title: 'Links', link: '/links' },
       { title: 'About', link: '/about' }
@@ -63,7 +62,12 @@ export const theme: ThemeUserConfig = {
     links: [
       // Registration link
       {
-        title: 'Moe ICP 114514',
+        title: '饿 ICP 4190',
+        link: 'https://beian.miit.gov.cn/',
+        style: 'text-xs text-muted-foreground' // Uno/TW CSS class
+      },
+      {
+        title: '萌 ICP 4190',
         link: 'https://icp.gov.moe/?keyword=114514',
         style: 'text-sm' // Uno/TW CSS class
       },
@@ -72,12 +76,6 @@ export const theme: ThemeUserConfig = {
         link: 'https://www.travellings.cn/go.html',
         style: 'text-sm'
       },
-      // Privacy Policy link
-      {
-        title: 'Site Policy',
-        link: '/terms/list',
-        pos: 2 // position set to 2 will be appended to copyright line
-      }
     ],
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
     credits: true,
